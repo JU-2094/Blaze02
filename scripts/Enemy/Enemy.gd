16f6fcd50bcae1536f7cd839339fff2a84510bc7
Josué Fabricio Urbina González <urbgon@gmail.com>
2019-01-27 23:46:38 -0600

    update enemy, still needs fix

--- scripts/Enemy/Enemy.gd
+++ scripts/Enemy/Enemy.gd
@@ -1,22 +1,38 @@
 extends KinematicBody2D
 export (int) var speed
 var intrud = KinematicBody2D.new()
-var isfollow= false
+var isfollow = false
+var istime =  false
+var follow_count
 var velocity = Vector2()
 var direction = Vector2()
+var origin_vector
+
 
 func _ready():
+	origin_vector = get_transform().origin
+	follow_count = 120
 	pass
 
 func _physics_process(delta):	
 	if isfollow:
 		enemydirection()
-		isfollow = false
+		# isfollow = false
+	elif istime and (follow_count > 0):
+		enemydirection()
+		follow_count -= 1
+	else:
+		returnorigin()
+		istime = false
+	if not isfollow:
+		follow_count -= 1
 	#position += velocity * delta
 	move_and_slide(velocity, Vector2(0,0))
 
 func _on_RayCast2D_follow(player):
 	isfollow = true
+	istime = true
+	follow_count = 120
 	intrud=player
 
 func _on_Vision_body_exited(body):
@@ -24,10 +40,20 @@ func _on_Vision_body_exited(body):
 		velocity.x = 0
 		velocity.y = 0
 		isfollow = false
+		istime = true
+		
 
 #function that updates the direction in wich the enemy is moving thowards
 func enemydirection():
 	velocity = intrud.get_transform().origin - self.get_transform().origin
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+
+func returnorigin():
+	velocity = origin_vector - self.get_transform().origin
+	if velocity.length() > 2:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
 
