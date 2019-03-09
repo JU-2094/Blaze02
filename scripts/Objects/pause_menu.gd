5e6d9074af1c63187eb17432cfb40e2ff01f2f37
Josué Fabricio Urbina González <urbgon@gmail.com>
2019-03-09 17:03:54 -0600

    Pause

---
+++ scripts/Objects/pause_menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+
+#	if Input.is_key_pressed(KEY_P) && worlddata.pause_state==true:
+##		worlddata.pause_state==false
+#		$pause_pop_up.hide()
+#		get_tree().paused = false
+	pass	
+
+func _input(event):
+	if  event.is_action_pressed("ui_pause") :
+		get_tree().paused = not get_tree().paused 
+		print("true")
+		if get_tree().paused:
+			$pause_pop_up.show()
+		else:
+			$pause_pop_up.hide()
+	pass
+
+func _on_Exit_pressed():
+	get_tree().quit()
+	pass # replace with function body
+
+
+func _on_Return_pressed():
+	print("entra button")
+	worlddata.pause_state=false
+	$pause_pop_up.hide()
+	get_tree().paused = false
+	pass # replace with function body
