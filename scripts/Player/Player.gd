d7a27a0c66f3a153c90c53f936e90b9e5baf39f8
JU-2094 <urbgon@gmail.com>
2019-05-19 18:19:29 -0500

    AnimationTree added to Player

--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -4,6 +4,8 @@ signal hit
 
 var speed=100
 var anim_obj 
+var anim_handler
+var anim_item_handler
 var screensize
 var isoverlay = false 
 var state_item
@@ -11,6 +13,20 @@ var orientation
 var screen_size = OS.get_real_window_size()
 var lock
 var lock_weapon
+
+func get_input():
+	var dir = \
+	{
+	"right" : (Input.is_action_pressed('ui_right') ), 
+	"left" : (Input.is_action_pressed('ui_left') ), 
+	"up" : (Input.is_action_pressed('ui_up') ), 
+	"down" : (Input.is_action_pressed('ui_down') ),
+	"right_rls" : (Input.is_action_just_released("ui_right")) ,
+	"left_rls" : (Input.is_action_just_released("ui_left")),
+	"up_rls" : (Input.is_action_just_released("ui_up") ),
+	"down_rls" : (Input.is_action_just_released("ui_down") )
+	}
+	return dir
 
 func _ready():
 	var canvas_transform = get_viewport().get_canvas_transform()
@@ -22,6 +38,10 @@ func _ready():
 	lock = 0
 	var anim_res = load('res://scripts/utils/animation.gd')
 	anim_obj = anim_res.new()
+	anim_handler = get_node("Sprite/AnimationPlayer/AnimationTree").get("parameters/playback")
+	anim_item_handler = get_node("Bomb/AnimationPlayer/AnimationTree").get("parameters/playback")
+	
+	anim_handler.start("idle_down")
 	lock_weapon = 0
 	# this avoid the user to be visible when the game starts
 	#hide()  
@@ -29,40 +49,114 @@ func _ready():
 func _process(delta):
 	check_health()
 	check_powerups()
+	var state_input = get_input()
 	var velocity = Vector2()
 	var state = 0
 		
-	if Input.is_action_pressed('ui_right') or Input.is_key_pressed(KEY_D):
+	if state_input["right"]:
 		velocity.x += 1
 		state += 1
 		orientation = 1
 		
-	if Input.is_action_pressed('ui_left') or Input.is_key_pressed(KEY_A):
+		if state_item:
+			anim_handler.travel("carry_right")
+		else:
+			anim_handler.travel("right")
+	if state_input["right_rls"]:
+		if state_item:
+			anim_handler.travel("idle_carry_right")
+		else:
+			anim_handler.travel("idle_right")
+		
+	if state_input["left"]:
 		velocity.x -= 1
 		state += 2
 		orientation = 2
+		if state_item:
+			anim_handler.travel("carry_left")
+		else:
+			anim_handler.travel("left")
+	if state_input["left_rls"]:
+		if state_item:
+			anim_handler.travel("idle_carry_left")
+		else:
+			anim_handler.travel("idle_left")
 		
-	if Input.is_action_pressed('ui_up') or Input.is_key_pressed(KEY_W):
+	if state_input["up"]:
 		velocity.y -= 1
 		state += 4
 		orientation = 3
+		if state_item:
+			anim_handler.travel("carry_up")
+		else:
+			anim_handler.travel("up")
+	if state_input["up_rls"]:
+		if state_item:
+			anim_handler.travel("idle_carry_up")
+		else:
+			anim_handler.travel("idle_up")
 		
-	if Input.is_action_pressed('ui_down') or Input.is_key_pressed(KEY_S):
+	if state_input["down"]:
 		velocity.y += 1
 		state += 8
 		orientation = 4
+		if state_item:
+			anim_handler.travel("carry_down")
+		else:
+			anim_handler.travel("down")
+	if state_input["down_rls"]:
+		if state_item:
+			anim_handler.travel("idle_carry_down")
+		else:
+			anim_handler.travel("idle_down")
 	
 	if Input.is_key_pressed(KEY_1):
 		if state_item == 0:
+			var node_item = get_node(playerdata.cur_item_res[playerdata.cur_item])
+			node_item.visible = true
 			$AudioStreamPlayer.play()
 			state_item = 1
+			anim_item_handler.start("bomb_up")
+			match orientation:
+				1:
+					anim_handler.travel("grab_right")
+				2:
+					anim_handler.travel("grab_left")
+				3:
+					anim_handler.travel("grab_up")
+				4:
+					anim_handler.travel("grab_down")
+				_:
+					anim_handler.travel("grab_down")
+			
+			
 	
 	if Input.is_key_pressed(KEY_SPACE):
-		if state_item == 2:
-			state_item = 4
+		if state_item:
+			match orientation:
+				1:
+					anim_item_handler.travel("throw_bomb_right")
+				2:
+					anim_item_handler.travel("throw_bomb_left")
+				3:
+					anim_item_handler.travel("throw_bomb_up")
+				4:
+					anim_item_handler.travel("throw_bomb_down")
+				_:
+					pass
+			
 		else:
-			lock_weapon = 1
-			 
+			match orientation:
+				1:
+					anim_handler.travel("sword_right")
+				2:
+					anim_handler.travel("sword_left")
+				3:
+					anim_handler.travel("sword_up")
+				4:
+					anim_handler.travel("sword_down")
+				_:
+					pass
 	if Input.is_key_pressed(KEY_B):
 		print(worlddata.prev_scene.get_name())
 		print(worlddata.current_scene.get_name())
@@ -71,10 +165,9 @@ func _process(delta):
 		velocity = velocity.normalized() * speed
 		
 	# Shortcut $AnimatedSprite --> get_node("AnimatedSprite")
-	if state_item == 0 or state_item == 2:
-		move_and_slide(velocity, Vector2(0,0))
-	
-	set_anim(state)
+	#if state_item == 0 or state_item == 2:
+	#	move_and_slide(velocity, Vector2(0,0))
+	move_and_slide(velocity, Vector2(0,0))
 
 # signal callback
 func _on_Player_body_entered(body):
@@ -93,9 +186,8 @@ func start(pos):
 	show()
 	$CollisionShape2D.disabled = false
 
-
 func _on_PlayerArea_area_entered(area):
-	print("hiot")
+	print("hit")
 	if area.get_name() != "Vision":
 		isoverlay = true
 	#if area.get_name() == "EnemyArea":
@@ -105,7 +197,7 @@ func _on_PlayerArea_area_entered(area):
 func _on_PlayerArea_area_exited(area):
 	isoverlay = false
 
-
+# TODO this needs refactor
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "grab_down":
 		if playerdata.items["bombs"] > 0:
@@ -115,89 +207,20 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	elif lock_weapon:
 		lock_weapon = 0
 
-func set_anim(state):
-	var stop = false
-	var node_player = $Sprite/AnimationPlayer
-	
-	var node_item = get_node(playerdata.cur_item_res[playerdata.cur_item])
-	if !state_item:
-		if !state:
-			stop = true
-		
-		# 0 - right, 1 - left, 2 - up, 3 - down 
-		match state:
-			1:
-				state = 0
-			2:
-				state = 1
-			4:
-				state = 2
-			8:
-				state = 3
-			9:
-				state = 3
-			10:
-				state = 3
-			_:
-				state = 2
-			# Se le suma 9 porque ahi empiezan las animaciones de la espada en playerdata
-		
-		if lock_weapon:
-			anim_obj.play_anim_item(orientation + 8, node_player, playerdata.anim["player"])
-		else:
-			if !lock_weapon:
-				anim_obj.play_anim(state, node_player, playerdata.anim["player"] , stop)
-	else:
-		match state_item:
-			1:
-				state = 8
-				anim_obj.play_anim_item(state, node_player, playerdata.anim["player"], playerdata.items[playerdata.cur_item], \
-										node_item, playerdata.anim[playerdata.cur_item], 0)
-			2:
-				if !state:
-					stop = true
-				# carry: 4 - right, 5 - left, 6 - up, 7 - down
-				match state:
-					1:
-						state = 4
-					2:
-						state = 5
-					4:
-						state = 6
-					8:
-						state = 7
-					9:
-						state = 7
-					10:
-						state = 7
-					_:
-						state = 6
-				anim_obj.play_anim(state, node_player, playerdata.anim["player"] , stop)
-			4:
-				if lock == 0:
-					node_player.stop()
-					anim_obj.play_anim(orientation, node_item.get_node('AnimationPlayer'), playerdata.anim[playerdata.cur_item])
-
-
+# TODO this need refactor
+"""
 func _on_AnimationBomb_finished(anim_name):
+	print('END OF ANIM')
 	if anim_name != "bomb_up":
+		print('here')
 		$Bomb.visible = false
 		$Bomb.position.x = 0
 		$Bomb.position.y = 0
 		$Bomb/AnimationPlayer.stop()
 		$Bomb/AudioStreamPlayer.play()
 		playerdata.items["bombs"] -=1
-		#items -= 1
 		state_item = 0
-		lock = 0
-
-
-
-func _on_Bomb_animation_started(anim_name):
-	if anim_name != "bomb_up":
-		lock = 1
-
-
+"""
 func _on_PlayerArea_body_entered(body):
 	print("DUH ",body.name)
 	if body.name == "Enemy":
@@ -232,3 +255,12 @@ func _on_timer_timeout():
 	timerpu.stop()
 	is_timerpu= true
 	speed=100
+
+func _on_Item_animation_finished(anim_name):
+	print("HERE!!")
+	pass # Replace with function body.
+
+
+func _on_Item_animation_started(anim_name):
+	print("INIT !! HERE!!")
+	pass # Replace with function body.
